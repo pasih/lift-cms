a618ad40697113a566bb412f8fa2959feb3dcfd6
Pasi Hirvonen <psh@iki.fi>
2013-01-21 13:37:25 +0200

    Fix incorrect behaviour when rendering non-complete fragments

--- src/main/scala/fi/paranoid/snippet/Content.scala
+++ src/main/scala/fi/paranoid/snippet/Content.scala
@@ -2,19 +2,22 @@ package fi.paranoid.snippet
 import fi.paranoid.lib.currentNode
 import net.liftweb.http.S
 import net.liftweb.common.Full
+import net.liftweb.common.Logger
 import net.liftweb.util.Html5
 import xml.NodeSeq
 
-class Content {
+class Content extends Logger {
   def render(in: NodeSeq): NodeSeq = {
     val node = currentNode.is
+    info("Looking for page fragment " + S.attr("areaId").getOrElse("<undefined fragment>"))
     S.attr("areaId") match {
       case Full(f) =>
         val fragment = for {
           fragment <- node.page.contentFragments.is.find(_.fragmentName.is == f)
         } yield fragment
         fragment match  {
-          case Some(a) => Html5.parse(a.contents.is) openOr <h1>Invalid content.</h1>
+          case Some(a) =>
+            Html5.parse("<div>" + a.contents.is + "</div>") openOr <h1>Invalid content.</h1>
           case _ => <h1>Could not find fragment '{ f }'</h1>
         }
 
