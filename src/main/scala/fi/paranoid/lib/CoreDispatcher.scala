cee1d3ab432f9bb2c6770ba820674ce189c72947
Pasi Hirvonen <psh@iki.fi>
2013-01-21 10:51:04 +0200

    Clean up page dispatch

--- src/main/scala/fi/paranoid/lib/CoreDispatcher.scala
+++ src/main/scala/fi/paranoid/lib/CoreDispatcher.scala
@@ -2,7 +2,7 @@ package fi.paranoid.lib
 
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.http._
-import net.liftweb.common.{Full, Empty, Box, Logger}
+import net.liftweb.common.{Box, Logger}
 
 object currentNode extends RequestVar[CmsNode](null)
 
@@ -16,18 +16,15 @@ object CoreDispatcher extends RestHelper with Logger {
   def doPageDispatch(node: CmsNode, r: Req): Box[LiftResponse] = {
     currentNode(node)
     info("Looking up page '%s'".format(node.page.title.is))
-
     for {
       session <- S.session
-      template <- Templates(List("cms-templates", node.page.template.is))
-      t = session.processSurroundAndInclude(node.page.title.is, template)
-      response <- session.processTemplate(Full(t), r, ParsePath(List("fooz"), // TODO get rid of fooz
-        "", absolute = false, endSlash = false), 200)
+      response <- session.processTemplate(Templates(List("cms-templates", node.page.template.is)),
+        r, ParsePath(List("fooz"), "", absolute = false, endSlash = false), 200)
     } yield response
   }
 
   serve {
-    case r @ Req(Finder(data), _, _) =>
-      doPageDispatch(data, r)
+    case r @ Req(Finder(node), _, _) =>
+      doPageDispatch(node, r)
   }
 }
